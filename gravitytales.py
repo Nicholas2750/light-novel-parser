37b6e6f2d0bfea7459f38fcbd93add89721d440d
Wilson E. Husin <wilsonehusin@gmail.com>
2018-12-30 17:34:56 -0600

    Basic linting and .gitignore

--- gravitytales.py
+++ gravitytales.py
@@ -2,47 +2,43 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers={'User-Agent':user_agent,} 
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' # noqa
+}
+
 
 def get_HTML(url):
-    request = urllib.request.Request(url,None,headers)
+    request = urllib.request.Request(url, None, headers)
     response = urllib.request.urlopen(request)
-    raw_HTML = response.read()
-
-    soup_HTML = BeautifulSoup(raw_HTML, features = 'html5lib')
-    
+    raw_HTML = response.read
+    soup_HTML = BeautifulSoup(raw_HTML, features='html5lib')
     return soup_HTML
 
 
-def get_chapter(HTML):
+def get_chapter(html_content):
     chapter = ''
-
-    for div in HTML.findAll('div', attrs = {'class': 'innerContent fr-view', 'id': 'chapterContent'}):
+    for div in html_content.findAll('div', attrs={'class': 'innerContent fr-vilew', 'id': 'chapterContent'}): # noqa
         chapter = div
-
     return str(chapter)
 
 
-def get_next_url(HTML, url):
+def get_next_url(html_content, url):
     next_url = ''
 
-    for a in HTML.findAll('a', attrs = {'class': 'btn btn-lg btn-link'}, href = True):
+    for a in html_content.findAll('a', attrs={'class': 'btn btn-lg btn-link'}, href=True): # noqa
         next_url = a['href']
-
     if next_url in url:
         return None
-    
     return next_url
 
 
-
-url = input("Enter the URL of the first chapter to parse: ")
-delay = int(input("Enter the delay time between each parsing: "))
+url = input('Enter the URL of the first chapter to parse: ')
+delay = int(input('Enter the delay time between each parsing: '))
 parsed = ''
 count = 1
 
-while url != None:
+while url:
     time.sleep(delay)
     HTML = get_HTML(url)
     next_url = get_next_url(HTML, url)
@@ -56,7 +52,7 @@ while url != None:
 
 parsed = '<html><body>' + parsed + '</body></html>'
 
-f = open('gravitytales_result.html','w')
+f = open('gravitytales_result.html', 'w')
 f.write(parsed)
 f.close()
 
--- wuxiaworld.py
+++ wuxiaworld.py
@@ -2,43 +2,40 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-headers={'User-Agent':user_agent,} 
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' # noqa
+}
+
 
 def get_HTML(url):
-    request = urllib.request.Request(url,None,headers)
+    request = urllib.request.Request(url, None, headers)
     response = urllib.request.urlopen(request)
-    raw_HTML = response.read()
-
-    soup_HTML = BeautifulSoup(raw_HTML, features = 'html5lib')
-    
+    raw_HTML = response.read
+    soup_HTML = BeautifulSoup(raw_HTML, features='html5lib')
     return soup_HTML
 
 
 def get_chapter(HTML):
-    title = ''
-    chapter = ''
-
+    raw_chapter = ''
     title = HTML.find('title').text
 
-    for div in HTML.findAll('div', attrs = {'class': 'fr-view'}):
-        if len(div) > len(chapter):
-            chapter = div
+    for div in HTML.findAll('div', attrs={'class': 'fr-view'}):
+        if len(div) > len(raw_chapter):
+            raw_chapter = div
 
-    chapter = str(chapter).replace('Previous Chapter', '').replace('Next Chapter', '')
+    chapter = str(raw_chapter).replace('Previous Chapter', '').replace('Next Chapter', '') # noqa
 
     return '<h4>' + title + chapter + '</h4>' + '<br>'
 
 
-def get_next_url(HTML):
+def get_next_url(html_content):
     next_url = ''
 
-    for a in HTML.findAll('a', attrs = {'class': 'btn btn-link'}, href = True):
+    for a in html_content.findAll('a', attrs={'class': 'btn btn-link'}, href=True): # noqa
         next_url = a['href']
-    
-    next_url = 'https://www.wuxiaworld.com' + next_url
 
-    return next_url
+    return 'https://www.wuxiaworld.com' + next_url
 
 
 url = input("Enter the URL of the first chapter to parse: ")
@@ -50,27 +47,23 @@ count = 1
 while True:
     try:
         time.sleep(delay)
-        HTML = get_HTML(url)
-        next_url = get_next_url(HTML)
-
-        parsed += get_chapter(HTML)
-
+        html_content = get_HTML(url)
+        next_url = get_next_url(html_content)
+        parsed += get_chapter(html_content)
         if prev_url == next_url:
             break
-
         prev_url = url
         url = next_url
 
         print(str(count) + ' chapters parsed')
         count += 1
-
-    except:
+    except Exception:
         break
 
 
 parsed = '<html><body>' + parsed + '</body></html>'
 
-f = open('wuxiaworld_result.html','w')
+f = open('wuxiaworld_result.html', 'w')
 f.write(parsed)
 f.close()
 
